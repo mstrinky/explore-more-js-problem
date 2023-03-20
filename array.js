cd98d1aa86bc1851dc072048a7175bafbb58ecbf
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-20 23:01:41 +0600

    cheaker

---
+++ array.js
@@ -0,0 +1,8 @@
+const mySchoolName = 'Islam nagar high school';
+console.log(typeof mySchoolName);
+const isYou = true;
+console.log(typeof isYou);
+const number = [3,4,5,56,3];
+console.log(number);
+const price = 45;
+console.log;
