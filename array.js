9db9b48c127e084ce5047bbe264210ee2074faae
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-20 23:17:06 +0600

    includes,concat etc

--- array.js
+++ array.js
@@ -3,6 +3,18 @@ console.log(typeof mySchoolName);
 const isYou = true;
 console.log(typeof isYou);
 const number = [3,4,5,56,3];
-console.log(number);
+// console.log(number);
+console.log(Array.isArray(number));
+console.log(number.includes(2));
 const price = 45;
-console.log;
+console.log(typeof price);
+
+
+// concat mane hocche onekgla string k akotre kora 
+const myFriends = ['a','b','c'];
+const yourFriends = ['d','e','e'];
+console.log(myFriends.concat(yourFriends));
+
+const atifAslam = 'dil kahe kahania pehli dafa , armano me rabania pehli dafa .'
+const tanveerEvan = 'seki jane aj tui kotha blis amr sathe mone mone .'
+console.log(atifAslam.concat(tanveerEvan));
